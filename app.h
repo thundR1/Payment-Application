6f14c0edba0921fd22b87ac54b9a07702bec43be
Youssef Amin <88247258+thundR1@users.noreply.github.com>
2022-11-22 22:45:25 +0200

    Delete app.h

--- app.h
+++
@@ -1,9 +0,0 @@
-#ifndef APP_H
-#define APP_H
-#include "../Payment_Application/card.h"
-#include "../Payment_Application/terminal.h"
-#include "../Payment_Application/server.h"
-
-void appStart(void);
-
-#endif
